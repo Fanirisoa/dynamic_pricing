e46626843364ce4fb3dcbd62b1624b657b382a96
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2019-10-04 07:16:49 +0200

    update new fonction methode

---
+++ estimationJob/GJR_GARCH/GJR_Gaussian_Esscher_returns/methode_return_GJR.R
@@ -0,0 +1,187 @@
+###################################################################################
+###               Clean the repertoir and laod all the library used         #######
+###################################################################################
+rm(list=ls())
+gc()
+library(compiler)
+enableJIT(1)
+enableJIT(3)
+library("fBasics")
+#library("pracma")
+library("numDeriv")
+library("nlme")
+library("Matrix")
+
+
+##################################################################################################
+###              Load : Data source,    Parameters of the model,  function to use          #######
+##################################################################################################
+setwd("/Users/leafanirisoa/Documents/projetGit/dynamic_pricing/data_used")  
+source("/Users/leafanirisoa/Documents/projetGit/dynamic_pricing/estimationJob/GJR_GARCH/GJR_Gaussian_Esscher_returns/parameters_settting.R")
+source("/Users/leafanirisoa/Documents/ProjectAll/Code_Pricing/Code_septembre_2019/Estimation_paper_3_2019/GJR_GARCH/GJR_Gaussian_Esscher_returns/Loglik_Return_GJR_sous_P.R")
+
+
+
+######################################################################################
+###               Volatility   plot under the initial parameters               #######
+######################################################################################
+
+ts.vol_P= shape_vol_P (para_h, Data.returns) 
+ts.plot(ts.vol_P , col = "steelblue", main = "IG Garch Model",xlab="2009",ylab="Volatility")
+grid()
+
+
+ts.vol_Q= shape_vol_Q (para_h, Data.returns) 
+ts.plot(ts.vol_Q, col = "steelblue", main = "IG Garch Model",xlab="2009",ylab="Volatility")
+grid()
+
+#####################################################
+###              LOg values returns           #######
+#####################################################
+start.time <- Sys.time()
+ILK=GJR_likelihood_ret(para_h, Data.returns)
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+ILK
+
+#####################################################
+###      Optimization  of the model           #######
+#####################################################
+start.time <- Sys.time()
+Sol=optim(para_h, GJR_likelihood_ret , Data.returns=Data.returns, method="Nelder-Mead",control = list(maxit = 5000))
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+Sol
+para_h1<-Sol$par
+
+
+# Standard error
+Hess=fdHess(para_h1,GJR_likelihood_ret, Data.returns=Data.returns)
+S_e <- sqrt(diag(solve(nearPD(Hess$Hessian)$mat)))
+S_e
+
+############################################################
+####                        RMSE                          ##
+############################################################
+
+N=8^5
+N
+path = "/Users/leafanirisoa/Documents/ProjectAll/Code_Pricing/Code_septembre_2019/Estimation_paper_3_2019/GJR_GARCH/GJR_Gaussian_Esscher_returns"
+source(paste(path,"/Function_Pricer_VIX_GJR.r",sep=""))
+source(paste(path,"/Simulation_MC.r",sep=""))
+source(paste(path,"/RMSE_VIX_GJR.r",sep=""))
+
+
+para_h <-para_h1 
+start.time <- Sys.time()
+RMSE1=RMSEsim(para_h1,Data.N)
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+RMSE1$rmse
+RMSE1$norm_rmse
+
+
+#####################################################
+###    Load both data.contract and data.ret   #######
+#####################################################
+load("DataPrice20112012.Rdata")
+
+Data.N=Data.N2
+############################################################
+####        RMSE  out of sample 2011-2012                 ##
+############################################################
+start.time <- Sys.time()
+RMSE2=RMSEsim(para_h1,Data.N2)
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+RMSE2$rmse
+RMSE2$norm_rmse
+
+
+############################################################
+####                       Compare VIX                    ##
+############################################################
+source("C:/Users/fanir/Desktop/Simulation_juin2018/GJR_GARCH/GJR_Gaussian_Esscher_returns/Comparing VIX_GJR_Garch.R")
+
+
+start.time <- Sys.time()
+C_VIX= Compa_vix(para_h1,Data.returns)
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+
+
+############################################################
+####                Simulation Price                      ##
+############################################################    
+start.time <- Sys.time()
+P_T_SimMC1=Pricer(N,para_h1,Data.N)
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+P_T_SimMC1$P
+P_T_SimMC1$Yt
+P_T_SimMC1$St
+P_T_SimMC1$St_Mar
+
+
+start.time <- Sys.time()
+P_T_SimMC1=Pricer(N,para_h1,Data.N2)
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+P_T_SimMC1$P
+
+
+############################################################
+####                        RMSE                          ##
+############################################################
+
+start.time <- Sys.time()
+RMSE2=RMSEsim (para_h1,Data.N)
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+RMSE2$rmse
+RMSE2$P
+
+
+
+##########################################################
+####                  DATA change                         ##
+############################################################
+
+start.time <- Sys.time()
+RMSE1=RMSE(para_h,Data.ret,Data.N)
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+RMSE1$rmse
+A=RMSE1$error 
+
+max(A)
+
+D1=c()    
+for(i in 1:length(A)) {
+  D1[i] = A[i]
+}
+
+
+Data.N<-data.frame(C=Data.N$C,K=Data.N$K,S=Data.N$S,T=Data.N$T,r=Data.N$r,d=Data.N$d,Pe=Data.N$Pe,Per=Data.N$Per,Mod=Data.N$Mod,CsK=Data.N$CsK,D1)
+
+
+Data.modif=Data.N[!Data.N$D1 <= 0.02,]
+Data.modif
+Data.N=Data.N[-c(506,1462,1638,1645),]
