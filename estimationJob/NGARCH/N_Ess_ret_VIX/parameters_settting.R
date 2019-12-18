26c8ae7d3525c10555ab5aeeec581a97e5bff6fd
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2019-12-18 10:07:35 +0100

    update new version

--- estimationJob/NGARCH/N_Ess_ret_VIX/parameters_settting.R
+++ estimationJob/NGARCH/N_Ess_ret_VIX/parameters_settting.R
@@ -188,39 +188,42 @@ legend(2^13,0.051,bty = "n", legend = c("IG_GARCH_RET_VIX_Us"), col = c("red"), 
 ############################################################
 RMSE_sim_in_IG_GARCH_RET_VIX_Us <- c(0.06113377,0.05925612, 0.05793751, 0.05753171, 0.05732707)
 
-plot(N_liste, RMSE_sim_in_IG_GARCH_RET_VIX_Us, type = "b", frame = FALSE, pch = 19,col ="red", ylim=c(0.05, 0.06),  xlim=c(2^10 - 15, 2^15 + 15), xlab = "number of simulation (N)", ylab =  "RMSE in of sample", lty = 1, lwd = 1)
+plot(N_liste, RMSE_sim_in_IG_GARCH_RET_VIX_Us, type = "b", frame = FALSE, pch = 19,col ="red", ylim=c(0.05, 0.06),  xlim=c(2^10 - 15, 2^15 + 15), xlab = "number of simulation (N)", ylab =  "RMSE out of sample", lty = 1, lwd = 1)
 legend(2^13,0.051,bty = "n", legend = c("IG_GARCH_RET_VIX_Us"), col = c("red"), lty = 1:4, cex = 0.8)
 
 
 
 
 
-  ## N_val = 2^11
-  ## 3.927617 mins
-  ## 0.06113377
-  ## 0.05406446
+## N_val = 2^11
+## 3.927617 mins
+## 0.06113377
+## 0.05406446
 
-  ## N_val = 2^12
-  ## 8.771856 mins
-  ## 0.05925612
-  ## 0.05240393
+## N_val = 2^12
+## 8.771856 mins
+## 0.05925612
+## 0.05240393
 
-  ## N_val = 2^13
-  ## 8.771856 mins
-  ## 0.05793751
-  ## 0.0512378
+## N_val = 2^13
+## 8.771856 mins
+## 0.05793751
+## 0.0512378
 
-  ## N_val = 2^14
-  ## 51.29552 mins
-  ## 0.05753171
-  ## 0.05087892
+## N_val = 2^14
+## 51.29552 mins
+## 0.05753171
+## 0.05087892
 
-  ## N_val = 2^15
-  ## 2.483147 hours
-  ## 0.05732707
-  ## 0.05069795
+## N_val = 2^15
+## 2.483147 hours
+## 0.05732707
+## 0.05069795
   
-  
+## N_val = 2^16
+## 16.46745 hours
+## 0.05723328
+## 0.050615
   
   
   
