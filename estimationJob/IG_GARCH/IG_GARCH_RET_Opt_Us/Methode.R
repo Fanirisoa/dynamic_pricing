97555ffa7987703b7ecbb671689357348882b0f6
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2019-12-26 04:46:36 +0100

    update news version

--- estimationJob/IG_GARCH/IG_GARCH_RET_Opt_Us/Methode.R
+++ estimationJob/IG_GARCH/IG_GARCH_RET_Opt_Us/Methode.R
@@ -1,4 +1,3 @@
-
 ###################################################################################
 ###               Clean the repertoir and laod all the library used         #######
 ###################################################################################
@@ -18,75 +17,15 @@ library(xts)
 ###              Load : Data source,    Parameters of the model,  function to use          #######
 ##################################################################################################
 setwd("/Users/leafanirisoa/Documents/projetGit/dynamic_pricing/data_used") 
-path = "/Users/leafanirisoa/Documents/projetGit/dynamic_pricing/estimationJob/IG_GARCH/IG_GARCH_RET_VIX_Us"
+path = "/Users/leafanirisoa/Documents/projetGit/dynamic_pricing/estimationJob/IG_GARCH/IG_GARCH_RET_Opt_Us"
 
+source(paste(path,"/parameters_settting.R",sep=""))
+source(paste(path,"/LoglikReturn.R",sep=""))
+source(paste(path,"/LoglikOprion.r",sep=""))
+source(paste(path,"/LoglikMix.R",sep=""))
+source(paste(path,"/RMSE_function_Us.R",sep=""))
+source(paste(path,"/Table_RMSE.R",sep=""))
 
-
-
-
-
-
-
-
-
-
-
-
-
-#####################################################
-###              Load Data source             #######
-#####################################################
-setwd("C:/Users/fanir/Desktop/Simulation_juin2018/Data")  
-
-#####################################################
-###             Clean the repertoir           #######
-#####################################################
-rm(list=ls())
-gc()
-library(compiler)
-enableJIT(1)
-enableJIT(3)
-library("fBasics")
-#library("pracma")
-library("numDeriv")
-library("nlme")
-library("Matrix")
-
-
-#####################################################
-###    Load both data.contract and data.ret   #######
-#####################################################
-load("DataPrice20092010.Rdata")
-
-#####################################################
-###                 Data set                  #######
-#####################################################
-#Data.N=Data.N2
-
-Data.N=Data.N2[-c(506,1462,1638,1645),]
-
-#####################################################
-###         Source function to use            #######
-#####################################################
-source("C:/Users/fanir/Desktop/Simulation_juin2018/IG-GARCH/IG_U_shape_returns_option/LoglikMix.R")
-source("C:/Users/fanir/Desktop/Simulation_juin2018/IG-GARCH/IG_U_shape_returns_option/LoglikOprion.r")
-source("C:/Users/fanir/Desktop/Simulation_juin2018/IG-GARCH/IG_U_shape_returns_option/LoglikReturn.r")
-
-#####################################################
-###         Parameters of the model           #######
-#####################################################
-##w=para_h[1]; b=para_h[2]; a=para_h[3];  c= para_h[4]; neta=para_h[5] ; nu=para_h[6]  ; PI=para_h[7]  
-
-###   Initial parameter  ####
-
-para_h<-c(4.238940e-06 , 2.058376e-03  ,3.317425e+03 , 5.058743e-05, -8.281782e-03 , 1.2584e+02, 1.100535e+00  )  ## RMSE2$rmse :   RMSE3$rmse :  
-
-para_h<-c(3.238940e-06 , 2.058376e-03  ,3.317425e+03 , 5.058743e-05, -8.281782e-03 , 1.2584e+02, 1.1003707e+00)
-
-para_h<-c(1.010747e-05,  2.282316e-03,  3.317425e+03,  4.514664e-05, -7.499894e-03,  1.258394e+02,  1.100010e+00) 
-
-###   Solution 
-para_h1<-c( 9.910995e-06 , 2.012356e-03,  3.317425e+03 , 4.785978e-05 ,-8.176758e-03 , 1.258400e+02 , 1.130528e+00) 
 
 #####################################################
 ###               Volatility                  #######
@@ -105,6 +44,9 @@ ILK=IGGARCH_likelihood_Mix(para_h,Data.ret, Data.N,Data.returns)
 end.time <- Sys.time()
 time.taken <- end.time - start.time
 time.taken
+
+ILK
+
 #####################################################
 ###      Optimization  of the model           #######
 #####################################################
@@ -115,7 +57,6 @@ time.taken <- end.time - start.time
 time.taken
 
 para_h1<-Sol$par
-
 para_h1=para_h
 
 # Standard error
@@ -134,6 +75,11 @@ time.taken
 
 RMSE1$rmse
 RMSE1$norm_rmse
+
+##############################
+###   Table Test GMM   #######
+##############################
+Table_RMSE(para_h=para_h1,Data.ret=Data.ret, Data.N=Data.N,N_val)
 
 
 #####################################################
@@ -175,3 +121,13 @@ end.time <- Sys.time()
 time.taken <- end.time - start.time
 time.taken
 Stand_error
+
+
+
+##############################
+###   Table Test GMM   #######
+##############################
+Table_RMSE(para_h=para_h,Data.ret=Data.ret, Data.N=Data.N,N_val)
+
+
+
