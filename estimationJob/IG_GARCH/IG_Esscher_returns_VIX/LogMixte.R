5e1c5eb8d24c17ce36207d0d46783dd8f40e39ca
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2019-12-31 10:12:32 +0100

    update new version

---
+++ estimationJob/IG_GARCH/IG_Esscher_returns_VIX/LogMixte.R
@@ -0,0 +1,9 @@
+#############################################################
+#####  tHE LOGLIKELIHOODE of both the mixe return-option ####
+#############################################################
+IGGARCH_likelihood_MixViX <- function(para_h,Data.returns) {
+
+  log_like= IGGARCH_likelihood_ret(para_h, Data.returns) + IGGARCH_likelihood_vix(para_h, Data.returns)
+  
+  return(-log_like)  
+}
