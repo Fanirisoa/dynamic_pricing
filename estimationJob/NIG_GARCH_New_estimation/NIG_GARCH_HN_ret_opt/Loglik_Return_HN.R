2b33bb2ea9fb0722445c91dae5e24514a12559b7
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2020-04-01 03:21:53 +0200

    update new version

--- estimationJob/NIG_GARCH_New_estimation/NIG_GARCH_HN_ret_opt/Loglik_Return_HN.R
+++ estimationJob/NIG_GARCH_New_estimation/NIG_GARCH_HN_ret_opt/Loglik_Return_HN.R
@@ -1,128 +1,82 @@
-####################################################
-######         The volatility updating rule       ##
-####################################################
-gsqrt <- function(para_h,ret,h,rt)
-{
-  ## set up the parameters of the model : para_h
-  a0=para_h[1]; a1=para_h[2]; gama=para_h[3];  b1= para_h[4] ;  lamda0= para_h[5]   
+########################################################################
+#      Fonction densite chaque loi conditionelle pour les returns      # 
+########################################################################
+# para_M = c(para_distribution,para_h) 
+densite <- function(para_M,l){
+  # alpha=para_distribution[1], beta=para_distribution[2], delta=para_distribution[3], mu=para_distribution[4]
+  # a0=para_h[1]; a1=para_h[2]; gama=para_h[3];  b1= para_h[4] ;  lamda0= para_h[5] 
   
-  # Parameter under the physical probability
-  # lamda0star= -(1/2)
-  # gamastar= gama+lamda0+(1/2)
+  ## set up the parameters of the model : para_M = c(para_distribution,para_h) 
+  alpha=para_M[1];  beta=para_M[2];  delta=para_M[3];  mu=para_M[4]
+  a0=para_M[5]; a1=para_M[6]; gama=para_M[7];  b1= para_M[8] ;  lamda0= para_M[9] 
   
+  gamma_0 = sqrt((alpha^2) -(beta^2))
   
-  #  h0=(a0 + a1)/(1 - b1 - a1*(gamastar)^2)    
-  h1=(a0 + a1)/(1 - b1 - a1*(gama)^2)    
-  # g0=b1 + a1*(gamastar)^2  
-  g1=b1 + a1*(gama)^2
+  ## Normalisation :
+  sigma_z = (delta*(alpha^2))/((gamma_0)^3)
+  
+  mu_z= mu - ((delta*beta)/(gamma_0 ))
+  
+  ## Parametrization : 
+  
+  alpha_1 = alpha*(sigma_z)
+  beta_1 =beta*(sigma_z)
+  delta_1 =delta/(sigma_z)
+  mu_1 =(1/(sigma_z))*(mu-mu_z)
+  
   
   drapeau=0
-  if (a0<=0){drapeau=1}
-  if (a1<=0){drapeau=1}
-  if (gama<=0){drapeau=1}
-  if (b1<=0){drapeau=1}
-  if (lamda0<=0){drapeau=1}
+  if (abs(alpha)<abs(beta)){drapeau=1}
+  if (alpha<=0){drapeau=1}
+  if (alpha==Inf){drapeau=1}
+  if (delta<=0){drapeau=1}
   
-  #  if (is.na(g0)==TRUE){drapeau=1}else{
-  #  if (g0>=1){drapeau=1}
-  #  if (abs(g0)==Inf){drapeau=1}
-  #  if (1/abs(g0)==Inf){drapeau=1}
-  #}
-  if (is.na(g1)==TRUE){drapeau=1}else{
-    if (g1>=1){drapeau=1}
-    if (abs(g1)==Inf){drapeau=1}
-    if (1/abs(g1)==Inf){drapeau=1}
-  }
-  #if (is.na(h0)==TRUE){drapeau=1}else{
-  # if (h0<=0){drapeau=1}
-  # if (abs(h0)==Inf){drapeau=1}
-  # if (1/abs(h0)==Inf){drapeau=1}
-  #}
-  if (is.na(h1)==TRUE){drapeau=1}else{
-    if (h1<=0){drapeau=1}
-    if (abs(h1)==Inf){drapeau=1}
-    if (1/abs(h1)==Inf){drapeau=1}
-  }
-  
-  if (is.na(h)==TRUE){drapeau=1}else{
-    if (h<=0){drapeau=1}
-    if (abs(h)==Inf){drapeau=1}
-    if (1/abs(h)==Inf){drapeau=1}
-  }
+  if (abs(alpha_1)<abs(beta_1)){drapeau=1}
+  if (alpha_1<=0){drapeau=1}
+  if (alpha_1==Inf){drapeau=1}
+  if (delta_1<=0){drapeau=1}
   
   if (drapeau==0){
-    resultat= a0 +b1*h+a1*(((ret-rt-lamda0*(h))/(sqrt(h))) - gama*(sqrt(h)))^2
+    resultat=dgh(l,alpha_1,beta_1,delta_1,mu_1,-1/2)
   }else{
     resultat=NA
   }
   return(resultat)
 }
 
-
-###########################################################
-#####  The conditional density of the daily return     ####
-###########################################################
-Retdensity <- function(para_h,Ret,h,r)
-{
-  ## set up the parameters of the model : para_h
-  a0=para_h[1]; a1=para_h[2]; gama=para_h[3];  b1= para_h[4] ;  lamda0= para_h[5]   
-  
-  
-  h0=(a0 + a1)/(1 - b1 - a1*(gama)^2)  
-  g1=b1 + a1*(gama)^2
-  
-  drapeau=0
-  if (a0<=0){drapeau=1}
-  if (a1<=0){drapeau=1}
-  if (gama<=0){drapeau=1}
-  if (b1<=0){drapeau=1}
-  if (lamda0<=0){drapeau=1}
-  
-  
-  if (is.na(g1)==TRUE){drapeau=1}else{
-    if (g1>=1){drapeau=1}
-    if (abs(g1)==Inf){drapeau=1}
-    if (1/abs(g1)==Inf){drapeau=1}
-  }
-  if (is.na(h)==TRUE){drapeau=1}else{
-   if (h<=0){drapeau=1}
-   if (abs(h)==Inf){drapeau=1}
-   if (1/abs(h)==Inf){drapeau=1}
-  }
-  if (is.na(h0)==TRUE){drapeau=1}else{
-    if (h0<=0){drapeau=1}
-    if (abs(h0)==Inf){drapeau=1}
-    if (1/abs(h0)==Inf){drapeau=1}
-  }
- 
-  if (drapeau==0){
-    resultat= (1/(sqrt(2*pi*h)))*exp((-1/2)*(((Ret-r-lamda0*h)^2)/h)) 
-  }else{
-    resultat=NA
-  }
-  return(resultat)
-}
-
-###########################################################
-#####  The Log-likeelihood over all the returns dates  ####
-###########################################################
-Heston_likelihood_ret <- function(para_h, Data.returns) {
+##########################################################
+#                likelihood function                     # 
+##########################################################  
+NIG_likelihood_dens_QML <- function(para_M,Data.returns) {
   ret=Data.returns$ret   
   rt=Data.returns$rt/250        
   Z1=length(rt)
-    
-  # para_h<-c() set up the parameters of the model 
-  a0=para_h[1]; a1=para_h[2]; gama=para_h[3];  b1= para_h[4] ;  lamda0= para_h[5]
+  
+  # alpha=para_distribution[1], beta=para_distribution[2], delta=para_distribution[3], mu=para_distribution[4]
+  # a0=para_h[1]; a1=para_h[2]; gama=para_h[3];  b1= para_h[4] ;  lamda0= para_h[5] 
+  
+  ## set up the parameters of the model : para_M = c(para_distribution,para_h) 
+  alpha=para_M[1];  beta=para_M[2];  delta=para_M[3];  mu=para_M[4]                   ## para_distribution<-c() set up the parameters of NIG
+  a0=para_M[5]; a1=para_M[6]; gama=para_M[7];  b1= para_M[8] ;  lamda0= para_M[9]     ## para_h<-c() set up the parameters of the model
+
   
   h = c()                                                        ####  A vector containing h from the model,
   h[1]=(a0 + a1)/(1 - b1 - a1*(gama)^2 )                         ####  The first value for h, Unconditional Variance
-  dens = Retdensity(para_h,ret[1],h[1],rt[1])
-    
+  z = c()                                                        ####  A vector containing z from the model,  innovation
+  z[1]=(ret[1]-rt[1]-lamda0*(h[1]))/sqrt(h[1])
+  
+  dens = densite(para_M,z[1])
+  
   for (i in 2:Z1){
-    h[i]= gsqrt(para_h,ret[i-1],h[i-1],rt[i-1]) ### a0 +b1*h[i-1]+a1*(((ret[i-1]-rt[i-1]-lamda0*(h[i-1]))/(sqrt(h[i-1]))) - gama*(sqrt(h[i-1])))^2
-    temp=Retdensity(para_h,ret[i],h[i],rt[i])
+    h[i]=a0 +b1*h[i-1]+a1*(((ret[i-1]-rt[i-1]-lamda0*(h[i-1]))/(sqrt(h[i-1]))) - gama*(sqrt(h[i-1])))^2
+    z[i]=(ret[i]-rt[i]-lamda0*(h[i]))/sqrt(h[i])
+    temp=densite(para_distribution,para_h,z[i])
     dens<-dens+log(temp)
   }
   
-  return(dens)  
+  return(-dens/length(dens))
 }
+
+
+
+
