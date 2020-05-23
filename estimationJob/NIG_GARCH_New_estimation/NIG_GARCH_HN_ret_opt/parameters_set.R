938f9075e4be9bec260971dc0c474b59c5fa46be
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2020-05-24 01:50:50 +0200

    update new version

--- estimationJob/NIG_GARCH_New_estimation/NIG_GARCH_HN_ret_opt/parameters_set.R
+++ estimationJob/NIG_GARCH_New_estimation/NIG_GARCH_HN_ret_opt/parameters_set.R
@@ -28,14 +28,14 @@ N= 2
 
 
 
-#para_h=c(1.854299e-04, 3.345238e-04 ,0.142406e+01 ,1.124012e-03 ,6.573458e-01)
-para_h<-c( 5.881028e-07,  1.506407e-06,  4.550518e+02,  6.500114e-01,  8.596182e+00)
+para_h=c(1.854299e-04, 3.345238e-04 ,0.142406e+01 ,1.124012e-03 ,6.573458e-01)
+#para_h<-c( 5.881028e-07,  1.506407e-06,  4.550518e+02,  6.500114e-01,  8.596182e+00)
 
 ###   Initial parameter  para_distribution<-c() set up the parameters of the distribution   ####
 ### alpha=para_distribution[1];  beta=para_distribution[2];  delta=para_distribution[3];  mu=para_distribution[4];
 
-para_distribution<-c(5, 1.397610234,   0.007012446,  -0.286886696)
-#para_distribution<-c(2.237095e+01, -1.047143e+01,  7.135328e-03,  8.048087e-04)
+#para_distribution<-c(5, 1.397610234,   0.007012446,  -0.286886696)
+para_distribution<-c(2.237095e+01, -1.047143e+01,  7.135328e-03,  8.048087e-04)
 
 
 para_M = c(para_distribution,para_h)
