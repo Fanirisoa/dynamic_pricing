d9cac3bbaa0d16967581143169f593ff955230d6
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2020-05-29 00:55:04 +0200

    update new version

---
+++ estimationJob/m_t_new/GJR_GARCH/GJR_NIG_Esscher_returns/Resulats.R
@@ -0,0 +1,30 @@
+> Sol
+$par
+[1]  7.5931976400  2.3311083208  0.0063598037  0.0001035206  0.0004206750 -0.0275364465 -0.5725617086 -0.2442607833  0.6372622181
+
+$value
+[1] -7985.664
+
+$counts
+function gradient 
+3378       NA 
+
+$convergence
+[1] 0
+
+$message
+NULL
+
+> para_M1
+[1]  7.5931976400  2.3311083208  0.0063598037  0.0001035206  0.0004206750 -0.0275364465 
+-0.5725617086 -0.2442607833  0.6372622181
+> para_M
+[1]  5.000000e+00  1.397610e+00  7.012446e-03 -2.868867e-01  5.987174e-06  1.240911e-01
+2.314265e-02  8.504269e-01  1.324430e+00
+> 
+1.035e-04  
+6.359e-03  
+5.725e-01
+## set up the parameters of the model : para_M = c(para_distribution,para_h) 
+##   alpha=para_M[1];  beta=para_M[2];  delta=para_M[3];  mu=para_M[4]                    ## para_distribution<-c() set up the parameters of NIG
+# a0=para_M[5]; a1=para_M[6]; a2=para_M[7];  b1= para_M[8] ;  lamda1= para_M[9]     
