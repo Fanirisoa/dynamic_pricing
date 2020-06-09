d39ec008a30b58503a8956053eb78da5819c6195
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2020-06-10 01:11:25 +0200

    update new version

---
+++ estimationJob/NIG_GARCH_Last/two_step_estimation/NIG_GARCH_HN_ret_vix/T2_Met_VIX_HN.R
@@ -0,0 +1,20 @@
+###################################################################################
+###               Clean the repertoir and laod all the library used         #######
+###################################################################################
+rm(list=ls())
+gc()
+library(compiler)
+enableJIT(1)
+enableJIT(3)
+library("fBasics")
+#library("pracma")
+library("numDeriv")
+library("nlme")
+library("Matrix")
+library(xts)
+
+##################################################################################################
+###              Load : Data source,    Parameters of the model,  function to use          #######
+##################################################################################################
+setwd("/Users/leafanirisoa/Documents/projetGit/dynamic_pricing/data_used")  
+path = "/Users/leafanirisoa/Documents/projetGit/dynamic_pricing/estimationJob/NIG_GARCH_Last/two_step_estimation/NIG_GARCH_HN_ret_Vix"
