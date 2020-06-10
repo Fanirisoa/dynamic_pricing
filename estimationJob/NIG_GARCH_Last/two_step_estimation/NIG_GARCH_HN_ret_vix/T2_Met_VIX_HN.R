a8eb4152b8ec53ef38eb2b5f80fa55d22faf5882
Fanirisoa <fanirisoa.zazaravaka@gmail.com>
2020-06-10 02:03:32 +0200

    update new version

--- estimationJob/NIG_GARCH_Last/two_step_estimation/NIG_GARCH_HN_ret_vix/T2_Met_VIX_HN.R
+++ estimationJob/NIG_GARCH_Last/two_step_estimation/NIG_GARCH_HN_ret_vix/T2_Met_VIX_HN.R
@@ -18,3 +18,69 @@ library(xts)
 ##################################################################################################
 setwd("/Users/leafanirisoa/Documents/projetGit/dynamic_pricing/data_used")  
 path = "/Users/leafanirisoa/Documents/projetGit/dynamic_pricing/estimationJob/NIG_GARCH_Last/two_step_estimation/NIG_GARCH_HN_ret_Vix"
+
+
+source(paste(path,"/T2_parameters_set.R",sep=""))
+source(paste(path,"/T2_Loglik_Ret_HN.R",sep=""))
+source(paste(path,"/T2_Loglik_VIX_HN.R",sep=""))
+source(paste(path,"/T2_Loglik_VIX_ret_HN.R",sep=""))
+source(paste(path,"/T2_QMLNIG_VIX_HN.R",sep=""))
+
+######################################################################################
+###               Volatility   plot under the initial parameters               #######
+######################################################################################
+
+ts.vol= shape_vol_P(para_h, Data.returns) 
+ts.plot(ts.vol, col = "steelblue", main = "IG Garch Model",xlab="2009",ylab="Volatility")
+grid()
+
+ts.vol= shape_vol_Q(para_h, Data.returns) 
+ts.plot(ts.vol, col = "steelblue", main = "IG Garch Model",xlab="2009",ylab="Volatility")
+grid()
+
+
+#####################################################
+###              Log values returns           #######
+#####################################################
+
+start.time <- Sys.time()
+ILK=Heston_likelihood_Mix(para_h,Data.ret, Data.N,Data.returns,N) 
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+ILK
+
+Heston_likelihood_ret(para_h, Data.returns)
+Heston_likelihood_vix(para_h,Data.returns,Data.ret) 
+
+
+#####################################################
+###      Optimization  of the model           #######
+#####################################################
+start.time <- Sys.time()
+Sol=optim(para_h,Heston_likelihood_Mix ,Data.ret=Data.ret, Data.N = Data.N,Data.returns=Data.returns, N=N, method="Nelder-Mead",control = list(maxit = 5000))
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+
+Sol
+para_h1<-Sol$par
+para_h
+para_h1
+
+##########################################################
+#                QML estimation  NIG                     # 
+##########################################################
+start.time <- Sys.time()
+QMLSol=optim(para_distribution,NIG_likelihood_dens_QML ,para_h =para_h1,Data.returns=Data.returns, method="Nelder-Mead",control = list(maxit = 5000))
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+time.taken
+QMLSol
+
+para_distribution1= QMLSol$par
+
+parametres_qml=c(para_h1,para_distribution1)
+
+
+
---
+++ estimationJob/NIG_GARCH_Last/two_step_estimation/NIG_GARCH_HN_ret_vix/T2_parameters_set.R
@@ -0,0 +1,117 @@
+#####################################################
+###    Load both data.contract and data.ret   #######
+#####################################################
+load("DataPrice20092010.Rdata")
+
+#####################################################
+###                 Data set                  #######
+#####################################################
+#Data.N=Data.N2
+
+Data.N=Data.N2[-c(506,1462,1638,1645),]
+
+#####################################################
+###      Parameters of the silulation         #######
+#####################################################
+N= 200
+
+
+#####################################################
+###             date to considere             #######
+#####################################################
+index_ht = which(Data.returns$date == "2010-01-04")
+index_vix = which(Data.ret$date == "2010-01-04")
+
+
+
+#####################################################
+###         Parameters of the model           #######
+#####################################################
+##   a0=para_h[1]; a1=para_h[2]; gama=para_h[3];  b1= para_h[4] ;  lamda0= para_h[5] ; ro=para_h[6]
+ 
+
+###   Initial parameter  ####``
+###   Initial parameter  para_h<-c() set up the parameters of the volatility  ####
+
+para_h<-c(1.180234e-12, 1.547729e-06, 4.550518e+02, 6.500111e-01 ,8.596182e+00, 9.646967e-01)
+          
+### para_h<-c(3.313135e-15, 1.366366e-06, 4.274284e+02, 7.324341e-01, 8.531595e+00, 9.95507e-01)  ## RMSE2$rmse : 0.0154167 ## RMSE3$rmse : 0.01615634
+### para_h<-c(3.788371e-13, 1.520721e-06, 4.649298e+02, 6.619980e-01, 4.400007e-01, 9.646967e-01)  ## RMSE2$rmse : 0.01082776 ## RMSE3$rmse : 0.03113633
+### para_h<-c(3.788371e-13, 1.520721e-06, 4.649298e+02, 6.619980e-01, 6.400007e-01, 9.646967e-01)  ## RMSE2$rmse : 0.0154167 ## RMSE3$rmse : 0.03113633
+### para_h<-c(1.905978e-08, 1.522443e-06, 4.642720e+02, 6.620030e-01, 6.399963e-01, 9.646983e-01) ## 0.01083887
+
+#para_h=c(1.854299e-04, 3.345238e-04 ,0.142406e+01 ,1.124012e-03 ,6.573458e-01)
+#para_h<-c( 5.881028e-07,  1.506407e-06,  4.550518e+02,  6.500114e-01,  8.596182e+00)
+
+###   Initial parameter  para_distribution<-c() set up the parameters of the distribution   ####
+### alpha=para_distribution[1];  beta=para_distribution[2];  delta=para_distribution[3];  mu=para_distribution[4];
+
+## #para_distribution<-c(5, 1.397610234,   0.007012446,  -0.286886696)
+
+## para_distribution<-c(2.237095e+01, -1.047143e+01,  7.135328e-03,  8.048087e-04)
+
+## para_distribution<-c(5, 1.397610234,   0.007012446,  -0.286886696)
+
+## para_distribution<-c(200.237095e+01 ,-1.047143e+01,  7.135328e-03 , 80.048087e-04) #### RMSE2$rmse: 0.01082957
+
+para_distribution=c(1.269943539, -0.002488772,  1.620073612,  2.088112106)
+
+
+# > #####################################################
+# > ###      Optimization  of the model           #######
+# > #####################################################
+# > start.time <- Sys.time()
+# > Sol=optim(para_h,Heston_likelihood_Mix ,Data.ret=Data.ret, Data.N = Data.N,Data.returns=Data.returns, N=N, method="Nelder-Mead",control = list(maxit = 5000))
+# > end.time <- Sys.time()
+# > time.taken <- end.time - start.time
+# > time.taken
+# Time difference of 6.537053 secs
+# > 
+#   > Sol
+# $par
+# [1] 4.283064e-07 1.537493e-06 4.607389e+02 6.510114e-01 8.596181e+00 9.646984e-01
+# 
+# $value
+# [1] -7788.42
+# 
+# $counts
+# function gradient 
+# 309       NA 
+# 
+# $convergence
+# [1] 0
+# 
+# $message
+# NULL
+# 
+# > para_h1<-Sol$par
+# > para_h
+# [1] 1.180234e-12 1.547729e-06 4.550518e+02 6.500111e-01 8.596182e+00 9.646967e-01
+# > para_h1
+# [1] 4.283064e-07 1.537493e-06 4.607389e+02 6.510114e-01 8.596181e+00 9.646984e-01
+# > ##########################################################
+# > #                QML estimation  NIG                     # 
+#   > ##########################################################
+# > start.time <- Sys.time()
+# > QMLSol=optim(para_distribution,NIG_likelihood_dens_QML ,para_h =para_h1,Data.returns=Data.returns, method="Nelder-Mead",control = list(maxit = 5000))
+# > end.time <- Sys.time()
+# > time.taken <- end.time - start.time
+# > time.taken
+# Time difference of 3.966922 secs
+# > QMLSol
+# $par
+# [1]  1.27567837 -0.05863686  1.52691996  2.29519637
+# 
+# $value
+# [1] 3552.764
+# 
+# $counts
+# function gradient 
+# 129       NA 
+# 
+# $convergence
+# [1] 0
+# 
+# $message
+# NULL
+# 
